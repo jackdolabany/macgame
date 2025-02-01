873aa54c7506f6c215c0aef01fec7b7b4ddd7f22
Jack Dolabany <jackdolabany@gmail.com>
2025-02-01 11:43:33 -0500

    World1 minecart level improvements

--- MacGame/Game1.cs
+++ MacGame/Game1.cs
@@ -16,7 +16,7 @@ namespace MacGame
     public class Game1 : Game
     {
 
-        public const string StartingWorld = "World2";
+        public const string StartingWorld = "World1MineCart";
         private const bool startAtTitleScreen = false;
         public const bool IS_DEBUG = true;
 

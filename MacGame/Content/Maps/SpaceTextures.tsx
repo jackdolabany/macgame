42fa59c2ab471790a0a4dd9503a0d66d8448d6d9
Jack Dolabany <jackdolabany@gmail.com>
2025-03-28 23:44:09 -0400

    adding some space level textures and a world. Nothing working yet.

---
+++ MacGame/Content/Maps/SpaceTextures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="SpaceTextures" tilewidth="8" tileheight="8" tilecount="576" columns="16">
+ <image source="../Textures/SpaceTextures.png" width="128" height="288"/>
+</tileset>

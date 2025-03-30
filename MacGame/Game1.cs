7227b1d8cbec477ed2a7d54fb4f53352c2ef4e06
Jack Dolabany <jackdolabany@gmail.com>
2025-03-29 20:54:28 -0400

    map tweak

--- MacGame/Game1.cs
+++ MacGame/Game1.cs
@@ -15,7 +15,7 @@ namespace MacGame
     public class Game1 : Game
     {
 
-        public const string StartingWorld = "HubWorld";
+        public const string StartingWorld = "World4Spaceship1";
         private const bool startAtTitleScreen = false;
         public const bool IS_DEBUG = true;
 

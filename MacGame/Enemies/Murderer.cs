315bfa9308d7855f4fedadca6a5c637ffee9e446
Jack Dolabany <jackdolabany@gmail.com>
2025-07-05 11:10:43 -0400

    some code for the murderer. Committing here so I can try stuff in cursor

--- MacGame/Enemies/Murderer.cs
+++ MacGame/Enemies/Murderer.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using MacGame.DisplayComponents;
+﻿using MacGame.DisplayComponents;
+using MacGame.Platforms;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using TileEngine;
 
 namespace MacGame.Enemies
 {
+    /// <summary>
+    /// Kinda like Jason from Friday the 13th, but certainly not him.
+    /// </summary>
     public class Murderer : Enemy
     {
         AnimationDisplay animations => (AnimationDisplay)DisplayComponent;
 
+        /// <summary>
+        /// He'll appear after some time.
+        /// </summary>
+        float appearTimer;
+
+        /// <summary>
+        /// This is the Rectangle that Mac needs to be inside for the murderer to appear.
+        /// In Tiled place a rectangle and call it "MurdererRectangle"
+        /// </summary>
+        Rectangle murdererRectangle;
+
+        private bool _isInitialized = false;
+        private Player _player;
+
         public Murderer(ContentManager content, int cellX, int cellY, Player player, Camera camera)
             : base(content, cellX, cellY, player, camera)
         {
+            _player = player;
+
             DisplayComponent = new AnimationDisplay();
 
             var textures = content.Load<Texture2D>(@"Textures\ReallyBigTextures");
@@ -42,11 +62,46 @@ namespace MacGame.Enemies
             IsAffectedByGravity = true;
 
             SetWorldLocationCollisionRectangle(10, 23);
+
+            this.Enabled = false;
+        }
+
+        private void Initialize()
+        {
+            // Find the murderer rectangle in the map.
+            foreach (var obj in Game1.CurrentMap.ObjectModifiers)
+            {
+                if (obj.Name == "MurdererRectangle")
+                {
+                    murdererRectangle = obj.GetScaledRectangle();
+                }
+            }
+
+            if (murdererRectangle == Rectangle.Empty)
+            {
+                throw new Exception("The murderer needs a rectangle named 'MurdererRectangle' on the map to know where to spawn.");
+            }
         }
 
         public override void Update(GameTime gameTime, float elapsed)
         {
-            
+
+            if (!_isInitialized)
+            {
+                Initialize();
+                _isInitialized = true;
+            }
+
+            if (Alive && !Enabled)
+            {
+                appearTimer += elapsed;
+                if (appearTimer > 2f)
+                {
+                    Enabled = true;
+                    appearTimer = 0f;
+                    animations.Play("walk");
+                }
+            }
 
             base.Update(gameTime, elapsed);
 

4afafda53a85bb7c456dd3e03def1c2893f6daed
Jack Dolabany <jackdolabany@gmail.com>
2025-07-06 10:44:45 -0400

    more murderer improvements

--- MacGame/Enemies/Murderer.cs
+++ MacGame/Enemies/Murderer.cs
@@ -15,10 +15,12 @@ namespace MacGame.Enemies
     {
         AnimationDisplay animations => (AnimationDisplay)DisplayComponent;
 
-        /// <summary>
-        /// He'll appear after some time.
-        /// </summary>
-        float appearTimer;
+        // Tracks how long the player has been inside the murderer rectangle
+        float playerInRectTimer = 0f;
+        float playerInRectTimerGoal = 3f;
+
+        // Margin in pixels to spawn off-screen
+        const int SpawnMargin = 32;
 
         /// <summary>
         /// This is the Rectangle that Mac needs to be inside for the murderer to appear.
@@ -28,6 +30,34 @@ namespace MacGame.Enemies
 
         private bool _isInitialized = false;
         private Player _player;
+
+        private MurdererState _state;
+
+        private float _speed = 60f;
+
+        // When attacking he'll jump every few seconds.
+        private float _jumpTimer = 0;
+        private float _jumpTimerGoal = 3;
+
+        // Don't turn more than once per second so he doesn't spaz.
+        private float _turnAroundTimer = 0f;
+        private float _turnAroundTimerGoal = 1f;
+
+        private float _throwSickleTimer = 0f;
+        private float _throwSickleTimerGoal = 4f;
+
+        /// <summary>
+        /// Sickle that he randomly throws.
+        /// </summary>
+        private Sickle _sickle;
+
+        private enum MurdererState
+        {
+            Hiding, 
+            Attacking,
+            Dying,
+            Dead
+        }
 
         public Murderer(ContentManager content, int cellX, int cellY, Player player, Camera camera)
             : base(content, cellX, cellY, player, camera)
@@ -44,14 +74,14 @@ namespace MacGame.Enemies
             animations.Add(walk);
 
             var toss = new AnimationStrip(textures, Helpers.GetReallyBigTileRect(2, 3), 1, "toss");
-            toss.LoopAnimation = true;
+            toss.LoopAnimation = false;
             toss.FrameLength = 0.14f;
             animations.Add(toss);
 
             // Face the player. Either to die or to turn around.
-            var face = new AnimationStrip(textures, Helpers.GetReallyBigTileRect(3, 3), 2, "face");
-            face.LoopAnimation = true;
-            face.FrameLength = 0.14f;
+            var face = new AnimationStrip(textures, Helpers.GetReallyBigTileRect(3, 3), 1, "face");
+            face.LoopAnimation = false;
+            face.FrameLength = 0.2f;
             animations.Add(face);
 
             animations.Play("walk");
@@ -60,10 +90,17 @@ namespace MacGame.Enemies
             Attack = 1;
             Health = 10;
             IsAffectedByGravity = true;
+            IsAbleToMoveOutsideOfWorld = true;
+            IsAbleToSurviveOutsideOfWorld = true;
+            CanBeJumpedOn = false;
 
             SetWorldLocationCollisionRectangle(10, 23);
 
             this.Enabled = false;
+            _state = MurdererState.Hiding;
+
+            _sickle = new Sickle(content, cellX, cellY, player, camera);
+            _sickle.Enabled = false;
         }
 
         private void Initialize()
@@ -85,26 +122,139 @@ namespace MacGame.Enemies
 
         public override void Update(GameTime gameTime, float elapsed)
         {
-
             if (!_isInitialized)
             {
                 Initialize();
                 _isInitialized = true;
             }
 
-            if (Alive && !Enabled)
+            // Only handle spawn logic if not enabled and alive
+            if (_state == MurdererState.Hiding)
             {
-                appearTimer += elapsed;
-                if (appearTimer > 2f)
+                // Check if player is inside the murderer rectangle
+                if (murdererRectangle.Contains(_player.CollisionCenter))
                 {
-                    Enabled = true;
-                    appearTimer = 0f;
-                    animations.Play("walk");
+                    playerInRectTimer += elapsed;
+                    if (playerInRectTimer >= playerInRectTimerGoal)
+                    {
+                        // Determine which side to spawn on
+                        int spawnX;
+
+                        // Y is tricky, just spawn him in the middle of the bounding rect, he's off screen so he'll just fall to the ground.
+                        int spawnY = murdererRectangle.Bottom - 8;
+
+                        // Figure out if the player is in the left, middle, or right third of the bounding rect
+                        bool isLeftSpawn = false;
+
+                        if (_player.WorldLocation.X < (murdererRectangle.X + (murdererRectangle.Width / 3f)))
+                        {
+                            // You're too close to the left, spawn on the right.
+                            isLeftSpawn = false;
+                        }
+                        else if (_player.WorldLocation.X > (murdererRectangle.X + murdererRectangle.Width * (2f / 3f)))
+                        {                             
+                            // You're too close to the right, spawn on the left.
+                            isLeftSpawn = true;
+                        }
+                        else
+                        {
+                            // You're in the middle, spawn is random.
+                            isLeftSpawn = Game1.Randy.NextBool();
+                        }
+
+                        if (isLeftSpawn)
+                        {
+                            // Player is to the right, spawn on left
+                            spawnX = Game1.Camera.ViewPort.X - SpawnMargin - (CollisionRectangle.Width / 2);
+                        }
+                        else
+                        {
+                            // Player is to the left, spawn on right
+                            spawnX = Game1.Camera.ViewPort.Right + SpawnMargin + (CollisionRectangle.Width / 2);
+                        }
+
+                        // Set position
+                        this.WorldLocation = new Vector2(spawnX, spawnY);
+                        Enabled = true;
+                        Alive = true;
+                        _state = MurdererState.Attacking;
+                        playerInRectTimer = 0f;
+                        animations.Play("walk");
+                    }
+                }
+                else
+                {
+                    playerInRectTimer = 0f;
                 }
             }
 
-            base.Update(gameTime, elapsed);
+            if (_state == MurdererState.Attacking)
+            {
+                // Walk towards the player
+                _turnAroundTimer += elapsed;
+                if (_turnAroundTimer >= _turnAroundTimerGoal)
+                {
+                    _turnAroundTimer = 0f;
+                    var oldFlipped = Flipped;
+                    Flipped = (Player.WorldLocation.X <= this.WorldLocation.X);
 
+                    if (oldFlipped != Flipped)
+                    {
+                        // If we flipped, play the face animation.
+                        animations.Play("face").FollowedBy("walk");
+                    }
+                }
+                
+                if (Flipped)
+                {
+                    Velocity = new Vector2(-_speed, Velocity.Y);
+                }
+                else
+                {
+                    Velocity = new Vector2(_speed, Velocity.Y);
+                }
+
+                // Jump at some interval.
+                _jumpTimer += elapsed;
+                if (_jumpTimer >= _jumpTimerGoal)
+                {
+                    Velocity = new Vector2(Velocity.X, -500);
+                    _jumpTimer = 0f;
+                }
+
+                // Check if the player is still in the rectangle
+                if (!murdererRectangle.Contains(_player.CollisionCenter))
+                {
+                    // Player left, go back to hiding
+                    _state = MurdererState.Hiding;
+                    Enabled = false;
+                    return;
+                }
+
+                // Throw a sickle at the player.
+                if (!_sickle.Enabled)
+                {
+                    _throwSickleTimer += elapsed;
+                    if (_throwSickleTimer >= _throwSickleTimerGoal)
+                    {
+                        _throwSickleTimer = 0f;
+                        // Spawn a sickle
+                        bool isToTheRight = _player.WorldLocation.X > this.WorldLocation.X;
+                        _sickle.Toss(this, isToTheRight);
+                    }
+                }
+
+            }
+
+            _sickle.Update(gameTime, elapsed);
+
+            base.Update(gameTime, elapsed);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            _sickle.Draw(spriteBatch);
         }
     }
 }
--- MacGame/Enemies/Sickle.cs
+++ MacGame/Enemies/Sickle.cs
@@ -14,7 +14,21 @@ namespace MacGame.Enemies
     {
         AnimationDisplay animations => (AnimationDisplay)DisplayComponent;
 
+        public float speed = 200f;
+        public float decelerationRate = 200f;
+        public float verticalSpeed = 25f;
+
+        // Sickle will move straight this distance and then start to move down and decelerate until it's at negative velocity and goes back.
+        public float moveStraightDistance = Game1.TileSize * 6;
+
+        Vector2 initialTossLocation;
+
         public bool IsBouncing { get; set; } = false;
+
+        public bool isComingBack = false;
+
+        GameObject _thrower;
+
         public Sickle(ContentManager content, int cellX, int cellY, Player player, Camera camera)
             : base(content, cellX, cellY, player, camera)
         {
@@ -24,7 +38,7 @@ namespace MacGame.Enemies
 
             var toss = new AnimationStrip(textures, Helpers.GetTileRect(7, 1), 4, "toss");
             toss.LoopAnimation = true;
-            toss.FrameLength = 0.14f;
+            toss.FrameLength = 0.1f;
             animations.Add(toss);
 
             isTileColliding = false;
@@ -33,7 +47,7 @@ namespace MacGame.Enemies
             Health = 1;
 
             IsAffectedByGravity = false;
-            IsAbleToSurviveOutsideOfWorld = true;
+            IsAbleToSurviveOutsideOfWorld = false;
             IsAbleToMoveOutsideOfWorld = true;
             CanBeHitWithWeapons = false;
             CanBeJumpedOn = false;
@@ -44,6 +58,47 @@ namespace MacGame.Enemies
         public override void Update(GameTime gameTime, float elapsed)
         {
             base.Update(gameTime, elapsed);
+
+            if (Enabled)
+            {
+                var distance = Math.Abs(initialTossLocation.X - this.worldLocation.X);
+                var isMovingRight = !Flipped;
+
+                if (distance > moveStraightDistance)
+                {
+
+                    isComingBack = true;
+
+                    // Start moving down and decelerating.
+                    Velocity = new Vector2(Velocity.X, verticalSpeed);
+
+                    if (isMovingRight)
+                    {
+                        Velocity = new Vector2(Velocity.X - decelerationRate * elapsed, Velocity.Y);
+                    }
+                    else
+                    {
+                        Velocity = new Vector2(Velocity.X + decelerationRate * elapsed, Velocity.Y);
+                    }
+                }
+                else
+                {
+                    velocity.Y = 0f;
+                }
+
+                // If it went it's initial distance, it dies when it's off camera or hits the thrower again
+                if (isComingBack)
+                {
+                    if (_thrower.CollisionRectangle.Contains(this.CollisionCenter))
+                    {
+                        this.Enabled = false;
+                    }
+                    if (camera.IsWayOffscreen(this.CollisionRectangle))
+                    {
+                        this.Enabled = false;
+                    }
+                }
+            }
         }
 
         public override void PlayDeathSound()
@@ -59,6 +114,26 @@ namespace MacGame.Enemies
                 Enabled = false;
             }
             base.Kill();
+        }
+
+        public void Toss(GameObject thrower, bool isToTheRight)
+        {
+            Enabled = true;
+            initialTossLocation = thrower.CollisionCenter;
+            animations.Play("toss");
+            this.WorldLocation = thrower.CollisionCenter;
+            if (isToTheRight)
+            {
+                Velocity = new Vector2(speed, 0);
+                Flipped = false;
+            }
+            else
+            {
+                Velocity = new Vector2(-speed, 0);
+                Flipped = true;
+            }
+            isComingBack = false;
+            _thrower = thrower;
         }
     }
 }

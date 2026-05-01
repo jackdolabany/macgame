147a7a0e6f3968245e7db2025569d12acfa0fa16
Jack Dolabany <jackdolabany@gmail.com>
2026-04-30 21:27:48 -0400

    Some work on the world 4 map

--- MacGame/Game1.cs
+++ MacGame/Game1.cs
@@ -16,7 +16,7 @@ namespace MacGame
     public class Game1 : Game
     {
 
-        public const string StartingWorld = "BigShipBossTest";
+        public const string StartingWorld = "World4";
         private const bool startAtTitleScreen = false;
         public const bool IS_DEBUG = true;
 
